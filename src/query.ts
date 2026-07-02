e88ad7ea3772d2e07fd54c2c060f12a21aa70fc3
Liam Ryde <liamryde@gmail.com>
2026-07-02 19:40:56 -0400

    Query adapters

---
+++ src/query.ts
@@ -0,0 +1,25 @@
+import type { StandardSchemaV1 } from "@standard-schema/spec";
+
+/**
+ * Query helper extensions attached to every server function.
+ * Compatible with TanStack Query (React Query, Vue Query, Solid Query).
+ */
+export interface ServerFnQueryExtensions<
+  TInput extends StandardSchemaV1,
+  TOutput extends StandardSchemaV1,
+> {
+  queryKey: (
+    ...args: StandardSchemaV1.InferInput<TInput> extends void
+      ? [input?: StandardSchemaV1.InferInput<TInput>]
+      : [input: StandardSchemaV1.InferInput<TInput>]
+  ) => [string, StandardSchemaV1.InferInput<TInput> | undefined];
+
+  queryOptions: (
+    ...args: StandardSchemaV1.InferInput<TInput> extends void
+      ? [input?: StandardSchemaV1.InferInput<TInput>]
+      : [input: StandardSchemaV1.InferInput<TInput>]
+  ) => {
+    queryKey: [string, StandardSchemaV1.InferInput<TInput> | undefined];
+    queryFn: () => Promise<StandardSchemaV1.InferOutput<TOutput>>;
+  };
+}
