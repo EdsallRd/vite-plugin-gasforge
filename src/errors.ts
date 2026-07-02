5a89e8126a465d9c7359565c8fa8eb50fadc2088
Liam Ryde <liamryde@gmail.com>
2026-07-02 19:41:07 -0400

    Improve error handling structure

---
+++ src/errors.ts
@@ -0,0 +1,24 @@
+import type { StandardSchemaV1 } from "@standard-schema/spec";
+
+export type GASForgeErrorCode =
+  | "INPUT_VALIDATION_FAILED"
+  | "OUTPUT_VALIDATION_FAILED"
+  | "MIDDLEWARE_ERROR"
+  | "SERVER_ERROR"
+  | "RPC_ERROR";
+
+export class GASForgeError extends Error {
+  public readonly code: GASForgeErrorCode;
+  public readonly issues?: ReadonlyArray<StandardSchemaV1.Issue>;
+
+  constructor(
+    code: GASForgeErrorCode,
+    message: string,
+    issues?: ReadonlyArray<StandardSchemaV1.Issue>,
+  ) {
+    super(message);
+    this.name = "GASForgeError";
+    this.code = code;
+    this.issues = issues;
+  }
+}
